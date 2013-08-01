2eea9c37ec9041e43af6840ab29660342b52f8b7
Antonio Romero <antonioromerooca@gmail.com>
2013-08-01 08:11:08 +0200

    Added shell date format configuration.

--- mysql_db_backup.sh
+++ mysql_db_backup.sh
@@ -3,17 +3,20 @@
 user=$1
 password=$2
 directory=$3
+[ -n "$4" ] && date="$4" || date="%Y-%m-%d_%H:%M:%S"
 
 connection="mysql -u$user -p$password"
 dbquery=$($connection -e "SHOW DATABASES;")
 
-db=( $( for dbname in $dbquery ; do echo $dbname ; done ) )
-
 cd $directory
-backup_name=$(date +%Y-%m-%d)
+
+backup_name="$(date +$date)"
+
 mkdir $backup_name
 cd $backup_name
 echo $backup_name
+
+db=( $( for dbname in $dbquery ; do echo $dbname ; done ) )
 
 for dbname in ${db[@]}; do
     if [[ "$dbname" != "Database" && \
